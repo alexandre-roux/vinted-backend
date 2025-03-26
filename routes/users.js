cda311b018a87d9e2f45171a6a2fcf2bd2f9445d
Alexandre Roux <alexandre.roux@ensc.fr>
2025-03-26 17:03:51 +0100

    Remove login errors

--- routes/users.js
+++ routes/users.js
@@ -54,9 +54,7 @@ router.post("/user/signup", async (req, res) => {
         }
 
         const salt = uid2(16);
-        console.log("password: " + req.fields.password + ", salt: " + salt);
-        const hash = SHA256(req.fields.password + salt);
-        console.log("hash: " + hash);
+        const hash = SHA256(req.fields.password + salt).toString();
         const token = uid2(16);
 
         // Create user
@@ -111,13 +109,13 @@ router.post("/user/login", async (req, res) => {
             return
         }
 
-        const user = await User.findOne({email: req.fields.email});
+        const user = await User.findOne({username: req.fields.username});
         if (!user) {
             res.status(404).json({error: {message: "User unknown"}});
             return;
         }
 
-        const hash = SHA256(req.fields.password + user.salt);
+        const hash = SHA256(req.fields.password + user.salt).toString();
         if (hash !== user.hash) {
             res.status(401).json({error: {message: "Unauthorized"}});
             return;
