bfa9251023b5644cb11a361d39d139c7042d5456
Alexandre Roux <alexandre.roux@ensc.fr>
2025-04-16 19:07:50 +0200

    Add signup mutation

--- graphql/Resolvers.js
+++ graphql/Resolvers.js
@@ -1,11 +1,12 @@
 const {SHA256} = require("crypto-js");
 const User = require("../models/User");
-const {loginSchema} = require("../validators/users");
+const {loginSchema, signupSchema} = require("../validators/users");
+const uid2 = require("uid2");
+const {v2: cloudinary} = require("cloudinary");
 
 const resolvers = {
     login: async ({email, password}) => {
-
-        const {error, value} = loginSchema.validate({email, password});
+        const {error} = loginSchema.validate({email, password});
         if (error) {
             throw new Error(error.details[0].message);
         }
@@ -26,6 +27,38 @@ const resolvers = {
             throw new Error(err.message);
         }
     },
+
+    signup: async ({email, username, password, phone}) => {
+        const {error} = signupSchema.validate({email, username, password, phone});
+        if (error) {
+            throw new Error(error.details[0].message);
+        }
+
+        try {
+            const existingUser = await User.findOne({email});
+            if (existingUser) {
+                throw new Error("A user is already registered with this email.");
+            }
+
+            const salt = uid2(16);
+            const hash = SHA256(password + salt).toString();
+            const token = uid2(16);
+
+            const newUser = new User({
+                email,
+                account: {username, phone},
+                token,
+                hash,
+                salt,
+            });
+
+            await newUser.save();
+
+            return newUser;
+        } catch (err) {
+            throw new Error(err.message);
+        }
+    }
 };
 
 module.exports = resolvers;
--- graphql/Schema.js
+++ graphql/Schema.js
@@ -11,9 +11,20 @@ const schema = buildSchema(`
         phone: String
         avatar: Avatar
     }
+    
+    type User {
+        _id: String!
+        email: String!
+        account: Account!
+        token: String!
+    }
 
     type Query {
         login(email: String!, password: String!): Account
+    }
+    
+    type Mutation {
+        signup(email: String!, username: String!, password: String!, phone: String): User
     }
 `);
 
